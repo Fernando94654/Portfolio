731d363cb6c32fe2050b19fb59746fb98d5693c2
Fernando94654 <A00841856@tec.mx>
2025-09-22 19:51:07 -0600

    fix pages importation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Intro from './(pages)/intro';
 import About from './(pages)/about';
-import Projects from './(pages)/workExperience';
+import WorkExperience from './(pages)/workExperience';
 import Contact from './(pages)/contact';
-import WorkExperience from './(pages)/projects';
+import Projects from './(pages)/projects';
 
 const Home = () => {
     return (
